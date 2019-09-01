bf96bb60e16a0e8256fd5125052cf7a8acc0e745
Jonah Nestrick <jonah.nestrick@gmail.com>
2019-09-01 08:16:57 +0000

    Added take/skipWhile and reduce functionality

---
+++ operators/takeWhile/index.js
@@ -0,0 +1,19 @@
+function takeWhile(predicate) {
+    return function* takeWhileGenerator(iterator) {
+        let index = -1;
+        for (const val of iterator) {
+            index += 1;
+
+            const result = predicate(val, index);
+
+            if (!result) {
+                return;
+
+            }
+
+            yield val;
+        }
+    };
+}
+
+module.exports = takeWhile;
